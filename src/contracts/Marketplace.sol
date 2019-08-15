d350be98fd502b8fe51cf422e7750c85d0b9f985
Iuwqyir <toomas.oosalu@gmail.com>
2019-08-15 12:21:16 +0100

    ADD product creation functionality

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -1,5 +1,30 @@
 pragma solidity ^0.5.0;
 
 contract Marketplace {
+	uint public productCount = 0;
+	mapping(uint => Product) public products;
 	
+	struct Product {
+		uint id;
+		string name;
+		uint price;
+		address owner;
+		bool purchased;
+	}
+
+	event ProductCreated(
+		uint id,
+		string name,
+		uint price,
+		address owner,
+		bool purchased
+	);
+
+	function createProduct(string memory _name, uint _price) public {
+		require(bytes(_name).length > 0);
+		require(_price > 0);
+		productCount++;
+		products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+		emit ProductCreated(productCount, _name, _price, msg.sender, false);
+	}
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,10 +1,14 @@
 const Marketplace = artifacts.require('./Marketplace.sol');
 
-contract('Marketplace', (accounts) => {
+require('chai')
+	.use(require('chai-as-promised'))
+	.should();
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
 	let marketplace;
 
 	before(async () => {
-		marketplace= await Marketplace.deployed();
+		marketplace = await Marketplace.deployed();
 	});
 
 	describe('deployment', async () => {
@@ -14,6 +18,33 @@ contract('Marketplace', (accounts) => {
 			assert.notEqual(address, '');
 			assert.notEqual(address, null);
 			assert.notEqual(address, undefined);
+		});
+	});
+
+	describe('product', async () => {
+		let result, productCount;
+
+		before(async () => {
+			result = await marketplace.createProduct("First Product", web3.utils.toWei('1', 'Ether'), { from: seller });
+			productCount = await marketplace.productCount();
+		});
+
+		it('creates products', async () => {
+			assert.equal(productCount, 1);
+			const event = result.logs[0].args;
+			assert.equal(event.id.toNumber(), productCount.toNumber(), 'ID is correct');
+			assert.equal(event.name, 'First Product', 'name is correct');
+			assert.equal(event.price, '1000000000000000000', 'price is correct');
+			assert.equal(event.owner, seller, 'owner is correct');
+			assert.equal(event.purchased, false, 'purchased is correct');
+		});
+
+		it('product must have a name', async () => {
+			await await marketplace.createProduct("", web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
+		});
+
+		it('product must have a price', async () => {
+			await await marketplace.createProduct("First Product", 0, { from: seller }).should.be.rejected;
 		});
 	});
 });
